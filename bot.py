58550c1587dbaea17a1c00d28449650ee37ee057
IamUnique2035 <101049823+IamUnique2035@users.noreply.github.com>
2022-03-06 23:12:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,34 +18,34 @@ async def bot(ctx):
         f'Hi ,I can do the following: \n convert fahrenheit to celcius by using  _F (value) and celcius to fahrenheit using  _C (value) \n clear messages in bulk(mod only command) using  _clear (value) the standard value is 10 messages \n convert the following currencies:usd,eur and inr to convert use _(currency you would like to convert from)_(currency you would like to convert to) (value)'
         f' ')
 
-
+#dumb easter egg command lol
 @client.command(aliases=['easteregg', 'Easteregg'])
 async def easter_egg(ctx):
     await ctx.send(f'Did you think it would be this easy ?')
 
-
+#part 2 of dumb easter egg command
 @client.command()
 async def EasterEgg(lol):
     await lol.send(f'Oh, I guess it was that easy.(easter egg 1/1 found)')
 
-
+# _ping tester
 @client.command(aliases=['test', 'Test'])
 async def ping(ctx):
     await ctx.send(f'pong {round(client.latency * 1000)}ms')
 
-
+#_clear (number of messages,if left blank amount =10)
 @client.command()
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount=10):
     await ctx.channel.purge(limit=amount)
 
-
+#dumb command lol
 @client.command(aliases=[
-    'suck dick', 'Suckdick', 'SuckDick', 'suckdick_kritharth', 'suck_dick', 'Suck_dick'])
+    'suck dick', 'Suckdick', 'SuckDick', 'suck_dick', 'Suck_dick'])
 async def suckdick(ctx):
     await ctx.send(f" ERROR. Can't find {ctx.author.mention}'s dick, object is missing or non-existent.")
 
-
+#if bot's nickname is changed to anything that includes the word porn it sets it back to original name
 @client.event
 async def on_member_update(before, after):
     n = after.nick
@@ -58,21 +58,7 @@ async def on_member_update(before, after):
                 await after.edit(nick="Kevin Jr")
 
 
-@client.command()
-async def adopted(ctx):
-    await ctx.send(f' OH NO THIS CANT BE TRUE')
-
-
-@client.command(aliases=['gi'])
-async def GI(ctx):
-    await ctx.send(f'Kevin has no interest in playing Genshin Impact')
-
-
-@client.command(aliases=['Protest'])
-async def protest(ctx):
-    await ctx.send(f'I WILL NOT.')
-
-
+                
 @client.command(aliases=['mad', 'Mad', 'Angry', 'Angy', 'angy'])
 async def angry(ctx):
     await ctx.send(f'https://tenor.com/view/angry-cat-instant-gif-24474761')
@@ -90,7 +76,7 @@ async def mood(ctx):
     ]
     await ctx.send(f'{random.choice(feels)}')
 
-
+#_8ball (question you would like to ask it)
 @client.command(aliases=['8ball', '8Ball'])
 async def _8ball(ctx, *, question):
     responses = [
@@ -114,7 +100,7 @@ async def created(ctx):
 async def horny(ctx):
     await ctx.send(f'https://tenor.com/view/horny-jail-bonk-dog-hit-head-stop-being-horny-gif-17298755')
 
-
+#_fight (user1) (user2)
 @client.command(aliases=['fight'])
 async def Fight(ctx, user1, *, user2):
     name1 = get(ctx.guild.members, name=user1)
@@ -124,7 +110,7 @@ async def Fight(ctx, user1, *, user2):
                 f'If I wanted to see two monkeys fighting each other I would have gone to the zoo, {name1.mention} & {name2.mention}')
     await ctx.send(f'{random.choice(response)}')
 
-
+#_nickname (@user1) (new nickname)
 @client.command(pass_content=True,
                 aliases=['Nickname', 'nickname', 'Changenick'])
 @commands.has_permissions(manage_nicknames=True)
@@ -138,26 +124,26 @@ async def changenick(ctx,
     await member.edit(nick=joint)
     await ctx.send(f'Nickname was changed for {member.mention}')
 
-
+#_load (cog to be reloaded) loads cogs
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
     await ctx.send(f'{extension}.py was loaded')
 
-
+#_restart (cog to be restarted)
 @client.command(aliases=['Restart'])
 async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
     await ctx.send(f'{extension}.py was reloaded')
 
-
+#unload (cog to be unloaded)
 @client.command()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     await ctx.send(f'{extension}.py was unloaded')
 
-
+#looks for the cog file. create a file named cogs at the same level of this file and create this file's cogs in it.
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')

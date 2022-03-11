a74dbba753a86ac7ec4c2925ea203306abaf9693
IamUnique2035 <101049823+IamUnique2035@users.noreply.github.com>
2022-03-11 19:21:48 +0530

    moved NASA API to its own file.

--- bot.py
+++ bot.py
@@ -13,48 +13,6 @@ client = commands.Bot(command_prefix='_', intents=discord.Intents.all())
 @client.event
 async def on_ready():
     print('Bot is ready.')
-
-#NASA API ( if you type _NASA it gives you nasa's image of the day) apply for a developer key at https://api.nasa.gov/)
-@client.command(aliases=['NASA','Nasa'])
-async def nasa(ctx, *, lmao=None):
-    apodurl = 'https://api.nasa.gov/planetary/apod?'
-    mykey = 'api_key=ur_API_key'
-    apodurlobj = urllib.request.urlopen(apodurl + mykey)
-    apodread = apodurlobj.read()
-    decodeapod = json.loads(apodread.decode('utf-8'))
-    # I wanted to display the image along with the title and an explanation. There is probably a better way to do this but it works! (lol)
-    await ctx.send (f"{decodeapod['hdurl']}")
-    await ctx.send(f"**{decodeapod['title']}**")
-    if lmao == None:
-        await ctx.send(f'if you want more info type "_NASA info"')
-    if lmao == "info":
-        await ctx.send(f"{decodeapod['explanation']}")
-    
-#bot that gives a random image a rover took along with the date and which rover took it. sometimes doesn't work since certain rovers didn't take photos on certain days.
-#also I'm terrible at naming variables
-@client.command()
-async def rover(ctx):
-    apodurl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/'
-    tros = '/photos?sol='
-    rovers = ["curiosity", "opportunity", "Spirit"]
-    lala = random.choice(rovers)
-    if lala == "Spirit":
-        sol = str(random.randrange(1, 764))
-    if lala == "opportunity":
-        sol = str(random.randrange(1, 1000))
-    if lala == "curiosity":
-        sol = str(random.randrange(99, 1000))
-    mykey = '&camera=fhaz&api_key=ur_API_key'
-    r = requests.get(apodurl + lala + tros + sol + mykey)
-    data = r.json()
-    neo = data['photos']
-    troup = neo[0]
-    rover = troup['rover']
-    name = rover['name']
-    oofd = troup['img_src']
-    date = troup['earth_date']
-    await ctx.send(f'{oofd}')
-    await ctx.send(f'taken on {date} by {name} rover')
     
     
 @client.command(aliases=['Help', 'HELP', 'Bot', 'BOT'])

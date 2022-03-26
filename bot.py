5b1211e8d97323eb13364daf49eed423eb646c35
IamUnique2035 <101049823+IamUnique2035@users.noreply.github.com>
2022-03-26 15:13:00 +0530

    removed unnecessary import commands

--- bot.py
+++ bot.py
@@ -2,10 +2,9 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import json
-import urllib.request
 import random
 import os
-import requests
+
 
 
 client = commands.Bot(command_prefix='_', intents=discord.Intents.all())
